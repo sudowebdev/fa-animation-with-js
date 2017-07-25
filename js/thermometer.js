fa2bf57cc3e77f9ec15b5b18b1bf8170bb342757
Nikhil Arora <narora200@gmail.com>
2017-07-25 19:13:49 +0530

    Thermometer: Thermometer (x4) animation structure completed

--- js/thermometer.js
+++ js/thermometer.js
@@ -19,4 +19,31 @@ emptyTheThermometer();
 setInterval(emptyTheThermometer, 2000);
 
 
+//script for (x4) thermometer animation
+let anotherNode = document.getElementById('node-2');
 
+function fillTheThermometer(){
+
+	anotherNode.innerHTML = '&#xf2cb;';
+
+	setTimeout(function(){
+		anotherNode.innerHTML = '&#xf2ca;';
+	}, 1000);
+
+	setTimeout(function(){
+		anotherNode.innerHTML = '&#xf2c9;';
+	}, 2000);
+
+	setTimeout(function(){
+		anotherNode.innerHTML = '&#xf2c8;';
+	}, 3000);	
+	
+	setTimeout(function(){
+		anotherNode.innerHTML = '&#xf2c7;';
+	}, 4000);	
+
+}
+
+fillTheThermometer();
+
+setInterval(fillTheThermometer, 5000);
